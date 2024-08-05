14c4ab9f03e17572bec0335cdefebe07bee0d85e
dvtcskn <117200113+dvtcskn@users.noreply.github.com>
2024-08-05 20:46:02 +0300

    Update

--- Content/Shaders/GBufferVS.hlsl
+++ Content/Shaders/GBufferVS.hlsl
@@ -24,6 +24,20 @@ struct GeometryVSIn
     uint ArrayIndex : ARRAYINDEX;
 };
 
+struct GeometryInstanceVSIn
+{
+    float3 position : POSITION;
+    float3 normal : NORMAL;
+    float2 texCoord : TEXCOORD;
+    float4 color : COLOR;
+    float3 tangent : TANGENT;
+    float3 binormal : BINORMAL;
+    uint ArrayIndex : ARRAYINDEX;
+
+    float3 InstancePos : INSTANCEPOS;
+    float4 InstanceColor : INSTANCECOLOR;
+};
+
 struct GeometryVSOut
 {
     float4 position : SV_Position;
@@ -51,6 +65,37 @@ GeometryVSOut GeometryVS(GeometryVSIn input)
     output.position = pos;
     
     output.color = input.color;
+    
+    output.normal = normalize(mul(float4(input.normal, 0), modelMatrix).xyz);
+    output.tangent = float4(input.tangent, 0.0f);
+    output.binormal = float4(input.binormal, 0.0f);
+	
+    output.texCoord = input.texCoord;
+    output.ArrayIndex = input.ArrayIndex;
+    
+    return output;
+}
+
+GeometryVSOut GeometryInstanceVS(GeometryInstanceVSIn input)
+{
+    GeometryVSOut output;
+    
+    float4 pos = float4(input.position.xyz, 1.0f);
+    pos.xyz = input.InstancePos.xyz + pos.xyz;
+#if WithModelMatrix
+    pos = mul(pos, modelMatrix);
+#endif
+    pos = mul(pos, mCameraWorldViewProj);
+	
+    pos.z = 0.0f;
+    pos.w = 1.0f;
+
+    output.position = pos;
+    
+    float4 color = input.color;
+    color.xyzw = input.InstanceColor.xyzw + color.xyzw;
+
+    output.color = color;
     
     output.normal = normalize(mul(float4(input.normal, 0), modelMatrix).xyz);
     output.tangent = float4(input.tangent, 0.0f);
